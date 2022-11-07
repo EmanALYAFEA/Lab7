96abcdc92074623816cb688ac00a521c0d567e91
EmanALYAFEA <112803673+EmanALYAFEA@users.noreply.github.com>
2022-11-07 12:04:03 +0300

    Add files via upload

---
+++ PersonTester.java
@@ -0,0 +1,12 @@
+public class  PersonTester {
+    public static void main(String[] args)
+    {
+        Person a = new Person("eman", 2003);
+        Student b = new Student("malak", 1999, "Information System");
+        Instructor c = new Instructor("aya", 2003, 95000);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
+
---
+++ TestOfFillin.java
@@ -0,0 +1,8 @@
+public class TestOfFillin {
+    public static void main(String[] args) {
+        FillinQuestion f=new FillinQuestion();
+        f.setText("MY NAME IS _eman_");
+        f.display();
+        System.out.println(f.getTextWithAnswer());
+
+    }}
